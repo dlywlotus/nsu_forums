9daba42d356ca035f87a03cd1f37fc14edb875d4
dlywlotus <dylanlowywgit@gmail.com>
2025-01-13 11:41:13 +0800

    fix: image cropper bug

--- client/src/util/getIcon.ts
+++ client/src/util/getIcon.ts
@@ -2,10 +2,14 @@ import supabase from "../supabase";
 import defaultIcon from "../images/defaultProfileIcon.svg";
 
 const getIcon = (profilePic: string | null) => {
-  return profilePic
-    ? supabase.storage.from("profile_icons").getPublicUrl(profilePic).data
-        .publicUrl
-    : defaultIcon;
+  if (profilePic?.startsWith("blob:")) {
+    return profilePic;
+  } else if (profilePic) {
+    return supabase.storage.from("profile_icons").getPublicUrl(profilePic).data
+      .publicUrl;
+  } else {
+    return defaultIcon;
+  }
 };
 
 export default getIcon;
